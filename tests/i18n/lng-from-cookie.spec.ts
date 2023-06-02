a05c858d381b2fce3d32eb7dff5399c0cb893a68
YaokunLin <yaokun1@ualberta.ca>
2023-06-02 06:38:46 -0700

    updated lng-from-cookie.spec.ts as per Shon comments

--- tests/i18n/lng-from-cookie.spec.ts
+++ tests/i18n/lng-from-cookie.spec.ts
@@ -1,197 +1,137 @@
-import { test, expect, chromium } from '@playwright/test';
+import { test, expect, chromium, BrowserContext, Page } from "@playwright/test";
 import { fallbackLng } from "../../app/i18n/settings";
-import { lngs, siteUrl } from './testUtils';
+import { lngs, siteUrl } from "./testUtils";
 
 const cookieName = "i18next";
 
-for (const lng of lngs) {
-  test(`Check that accessing URL with ${lng} prefix 
-  sets the i18next language preference cookie`, async ({ page }) => {
-    // Go to the page with language prefix
-    await page.goto(`${siteUrl}/${lng}`);
-  
-    // Get cookies
-    const cookies = await page.context().cookies();
+test.describe("Server Language Response Tests", () => {
+  let context: BrowserContext;
 
-    // Find i18next cookie
-    const i18nCookie = cookies.find(cookie => cookie.name === 'i18next');
-
-    // Assert that the cookie exists and its value matches the language prefix
-    expect(i18nCookie).toBeTruthy();
-    expect(i18nCookie?.value).toBe(lng);
-  });
-}
-
-
-for (const lng of lngs) {
-  test(`Ensure i18next language preference cookie is set and 
-  verify that a URL, without language prefix, 
-  displays content in the i18next language preference: ${lng}`, async ({ page }) => {
-    // Set i18next cookie
-    await page.context().addCookies([{
-      name: 'i18next',
-      value: lng,
-      url: siteUrl,
-    }]);
-    
-    // Navigate to the page without language prefix
-    await page.goto(siteUrl);
-    
-    // Check that the page URL matches the expected language URL
-    expect(page.url()).toContain(`/${lng}`);
-
-    // Get cookies
-    const cookies = await page.context().cookies();
-    // Find i18next cookie
-    const i18nCookie = cookies.find(cookie => cookie.name === 'i18next');
-
-    // Assert that the cookie exists and its value matches the language
-    expect(i18nCookie).toBeTruthy();
-    expect(i18nCookie?.value).toBe(lng);
-  });
-}
-
-for (const lng of lngs) {
-  test(`Clear the cookie and 
-  ensure that a url, without language prefix, 
-  sets the i18next language preference cookie to 
-  the value of the Accept-Language header default language: ${lng}`, async () => {
-    // Launch browser
+  test.beforeAll(async () => {
     const browser = await chromium.launch();
+    context = await browser.newContext();
+  });
 
-    // Create a new context with the Accept-Language header set to the current language
-    const context = await browser.newContext({
-      locale: lng
+  test.afterAll(async () => {
+    await context.close();
+  });
+
+  const testCases = [
+    {
+      description:
+        "Check that accessing URL with language prefix sets the i18next language preference cookie",
+      testFn: async (page: Page, lng: string) => {
+        await page.goto(`${siteUrl}/${lng}`);
+        const cookies = await page.context().cookies();
+        const i18nCookie = cookies.find((cookie) => cookie.name === cookieName);
+        expect(i18nCookie).toBeTruthy();
+        expect(i18nCookie?.value).toBe(lng);
+      },
+    },
+    {
+      description:
+        "Ensure i18next language preference cookie is set and verify that a URL, without language prefix, displays content in the i18next language preference",
+      testFn: async (page: Page, lng: string) => {
+        await page.context().addCookies([
+          {
+            name: cookieName,
+            value: lng,
+            url: siteUrl,
+          },
+        ]);
+        await page.goto(siteUrl);
+        expect(page.url()).toContain(`/${lng}`);
+        const cookies = await page.context().cookies();
+        const i18nCookie = cookies.find((cookie) => cookie.name === cookieName);
+        expect(i18nCookie).toBeTruthy();
+        expect(i18nCookie?.value).toBe(lng);
+      },
+    },
+    {
+      description:
+        "Clear the cookie and ensure that a URL, without language prefix, sets the i18next language preference cookie to the value of the Accept-Language header default language",
+      testFn: async (page: Page, lng: string) => {
+        const browser = await chromium.launch();
+        const context = await browser.newContext({
+          locale: lng,
+        });
+        page = await context.newPage();
+        await page.goto(siteUrl);
+        const cookies = await context.cookies();
+        const i18nCookie = cookies.find((cookie) => cookie.name === cookieName);
+        expect(i18nCookie).toBeTruthy();
+        expect(i18nCookie?.value).toBe(lng);
+        await browser.close();
+      },
+    },
+  ];
+
+  testCases.forEach(({ description, testFn }) => {
+    lngs.forEach((lng) => {
+      test(description + `: ${lng}`, async ({ page }) => {
+        await testFn(page, lng);
+      });
     });
+  });
 
-    // Create a new page in this context
+  test(`Clear the cookie and ensure that a url, without language prefix, sets the i18next language preference cookie to the value of the i18next default language when the Accept-Language header is NOT available or NOT supported`, async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+      locale: "zh-CN",
+    });
     const page = await context.newPage();
-
-    // Navigate to the page without language prefix
     await page.goto(siteUrl);
-
-    // Get cookies
     const cookies = await context.cookies();
-    // Find i18next cookie
-    const i18nCookie = cookies.find(cookie => cookie.name === 'i18next');
-
-    // Assert that the cookie exists and its value matches the language
+    const i18nCookie = cookies.find((cookie) => cookie.name === cookieName);
     expect(i18nCookie).toBeTruthy();
-    expect(i18nCookie?.value).toBe(lng);
-
-    // Close the browser at the end of the test
+    expect(i18nCookie?.value).toBe(fallbackLng);
     await browser.close();
   });
-}
 
-test(`Clear the cookie and 
-ensure that a url, without language prefix, 
-sets the i18next language preference cookie to 
-the value of the i18next default language when 
-the Accept-Language header is NOT available or NOT supported`, async () => {
-  // Launch browser
-  const browser = await chromium.launch();
-
-  // Create a new context with the Accept-Language header set to a non-supported language
-  const context = await browser.newContext({
-    locale: 'zh-CN' // Chinese, which is not supported by our i18next languages
+  test(`Missing i18next cookie should use Accept-Language header`, async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+      locale: "fr-CA",
+    });
+    const page = await context.newPage();
+    await page.goto(siteUrl);
+    await expect(page).toHaveURL(/.*fr-CA/);
+    await browser.close();
   });
 
-  // Create a new page in this context
-  const page = await context.newPage();
-
-  // Navigate to the page without language prefix
-  await page.goto(siteUrl);
-
-  // Get cookies
-  const cookies = await context.cookies();
-  // Find i18next cookie
-  const i18nCookie = cookies.find(cookie => cookie.name === 'i18next');
-
-  // Assert that the cookie exists and its value matches the fallback language
-  expect(i18nCookie).toBeTruthy();
-  expect(i18nCookie?.value).toBe(fallbackLng); 
-
-  // Close the browser at the end of the test
-  await browser.close();
-});
-
-test(`Missing i18next cookie should use Accept-Language header`, async () => {
-  // Launch browser
-  const browser = await chromium.launch();
-
-  // Create a new context with the Accept-Language header set to 'fr-CA'
-  const context = await browser.newContext({
-    locale: 'fr-CA'
+  test(`Invalid i18next cookie should use Accept-Language header`, async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+      locale: "fr-CA",
+    });
+    const page = await context.newPage();
+    await context.addCookies([
+      {
+        name: cookieName,
+        value: "unsupported-language",
+        url: siteUrl,
+      },
+    ]);
+    await page.goto(siteUrl);
+    await expect(page).toHaveURL(/.*fr-CA/);
+    await browser.close();
   });
 
-  // Create a new page in this context
-  const page = await context.newPage();
-
-  // Navigate to the page without language prefix
-  await page.goto(siteUrl);
-
-  // Expect URL to have '/fr-CA/' indicating redirection to French Canadian language
-  await expect(page).toHaveURL(/.*fr-CA/);
-
-  // Close the browser at the end of the test
-  await browser.close();
-});
-
-test(`Invalid i18next cookie should use Accept-Language header`, async () => {
-  // Launch browser
-  const browser = await chromium.launch();
-
-  // Create a new context with the Accept-Language header set to 'fr-CA'
-  const context = await browser.newContext({
-    locale: 'fr-CA'
+  test(`Conflict between i18next cookie and Accept-Language header should prioritize i18next cookie`, async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+      locale: "fr-CA",
+    });
+    const page = await context.newPage();
+    await context.addCookies([
+      {
+        name: cookieName,
+        value: "en-GB",
+        url: siteUrl,
+      },
+    ]);
+    await page.goto(siteUrl);
+    await expect(page).toHaveURL(/.*en-GB/);
+    await browser.close();
   });
-
-  // Create a new page in this context
-  const page = await context.newPage();
-
-  // Set invalid 'i18next' cookie
-  await context.addCookies([{
-    name: cookieName,
-    value: 'unsupported-language',
-    url: siteUrl,
-  }]); 
-
-  // Navigate to the page without language prefix
-  await page.goto(siteUrl);
-
-  // Expect URL to have '/fr-CA/' indicating redirection to French Canadian language
-  await expect(page).toHaveURL(/.*fr-CA/);
-
-  // Close the browser at the end of the test
-  await browser.close();
-});
-
-test(`Conflict between i18next cookie and Accept-Language header should prioritize i18next cookie`, async () => {
-  // Launch browser
-  const browser = await chromium.launch();
-
-  // Create a new context with the Accept-Language header set to 'fr-CA'
-  const context = await browser.newContext({
-    locale: 'fr-CA'
-  });
-
-  // Create a new page in this context
-  const page = await context.newPage();
-
-  // Set 'i18next' cookie to 'en'
-  await context.addCookies([{
-    name: cookieName,
-    value: 'en',
-    url: siteUrl,
-  }]);
-
-  // Navigate to the page without language prefix
-  await page.goto(siteUrl);
-
-  // Expect URL to have '/en/' indicating redirection to English language
-  await expect(page).toHaveURL(/.*en/);
-
-  // Close the browser at the end of the test
-  await browser.close();
 });
