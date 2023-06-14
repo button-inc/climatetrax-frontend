408ab7668505d7dd9e1b8a9a7b6818ef47d1f688
YaokunLin <yaokun1@ualberta.ca>
2023-06-13 23:38:40 -0700

    implemented keyloak

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,5 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import KeycloakProvider from "next-auth/providers/keycloak";
+import jwt from 'jsonwebtoken';
 
 export const authOptions: NextAuthOptions = {
   pages: {
@@ -13,19 +15,36 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    KeycloakProvider({
+      clientId: "ClimateTrax-Client",
+      clientSecret: "KZ5zsX6vIEWBtNw2iPqCD9ZO4Jv4C2Y6",
+      issuer: "http://localhost:8080/realms/ClimateTrax",
+    }),
+    
   ],
   callbacks: {
     session: ({ session, token }) => {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: token.id,
-          role: token.role,
-        },
-      };
-    },
-    jwt: ({ token, user }) => {
+    let rolesKeycloak;
+    if (token?.access_token) {
+      const data = jwt.decode(token.access_token);
+      const authorizedParty = data.azp;
+      rolesKeycloak = data.resource_access[authorizedParty]?.roles;
+    }
+
+    return {
+      ...session,
+      user: {
+        ...session.user,
+        id: token.id,
+        role: token.role,
+        rolesKeycloak: rolesKeycloak,
+      },
+    };
+   },
+    jwt: ({ token, user, account }) => {
+      if (account?.access_token) {
+        token.access_token = account.access_token;
+      }
       if (user) {
         const u = user as unknown as any;
         return {
