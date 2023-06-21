b31c2b5270b4c2b39fa1d593659519f2a5e07d68
shon-button <shon@button.is>
2023-06-21 15:58:19 -0400

    update to const authOptions

--- app/utils/auth/auth.ts
+++ app/utils/auth/auth.ts
@@ -1,5 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import KeycloakProvider from "next-auth/providers/keycloak";
 
 export const authOptions: NextAuthOptions = {
   pages: {
@@ -13,15 +14,32 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID as string,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET as string,
+      issuer: ((process.env.KEYCLOAK_AUTH_URL as string) +
+        process.env.KEYCLOAK_AUTH_REALM) as string,
+    }),
   ],
   callbacks: {
     session: ({ session, token }) => {
+      let rolesKeycloak;
+      if (token?.access_token) {
+        //👇️by utilizing the jwt object provided by NextAuth, you can achieve the desired functionality without importing the jsonwebtoken package
+        const data = token.jwt as {
+          azp: string;
+          resource_access: Record<string, { roles?: string[] }>;
+        };
+        const authorizedParty = data.azp;
+        rolesKeycloak = data.resource_access[authorizedParty]?.roles;
+      }
+
       return {
         ...session,
         user: {
           ...session.user,
           id: token.id,
-          role: "analyst" //token.role, 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 
+          role: "analyst", //token.role, 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨
         },
       };
     },
@@ -31,7 +49,7 @@ export const authOptions: NextAuthOptions = {
         return {
           ...token,
           id: u.id,
-          role: "analyst" //u.role, 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 
+          role: "analyst", //u.role, 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨 🚨
         };
       }
       return token;
