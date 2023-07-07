4e1fc7ef2f582642b0d9d2e697804dc910efa6a8
YaokunLin <yaokun1@ualberta.ca>
2023-07-07 11:49:10 -0700

    gitleaks debugging echo

--- .bin/install-gitleaks-linux-x64.sh
+++ .bin/install-gitleaks-linux-x64.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -xeuo pipefail;
 
-releases_api="https://api.github.com/repositories/119190187/releases/latest";
-releases_json="$(curl -s ${releases_api})";
-version=$(echo "${releases_json}" | jq -r ".name" | cut -c 2-); #Extracts the version from the json
+releases_api="https://api.github.com/repos/gitleaks/gitleaks/releases/latest"
+releases_json="$(curl -s ${releases_api})"
+version=$(echo "${releases_json}" | jq -r ".name" | cut -c 2-) #Extracts the version from the json
 
 # Detect OS architecture and set the arch variable
 case "$(uname -m)" in
@@ -29,8 +29,23 @@ esac
 # Construct the filename to search for in the API response
 filename="gitleaks_${version}_${os}_${arch}.zip"
 
+# Debugging lines: echo variables
+echo "Version: ${version}"
+echo "OS: ${os}"
+echo "Arch: ${arch}"
+echo "Filename: ${filename}"
+
 # Find the download URL for the desired asset
 download_url=$(echo "${releases_json}" | jq -r ".assets[] | select(.name==\"${filename}\") | .browser_download_url")
+
+# Check if download URL is found
+if [ -z "${download_url}" ]; then
+  echo "Could not find a download URL for ${filename}"
+  exit 1
+fi
+
+# Echo the download URL
+echo "Download URL: ${download_url}"
 
 # Download the asset
 wget "${download_url}"
@@ -39,6 +54,6 @@ wget "${download_url}"
 unzip "gitleaks_${version}_${os}_${arch}.zip"
 
 # Cleanup
-rm gitleaks_${version}_${os}_${arch}.zip
+rm "gitleaks_${version}_${os}_${arch}.zip"
 
 exit 0;
