920a2483f77d0832e2940b088818780681ab8df7
YaokunLin <yaokun1@ualberta.ca>
2023-07-07 11:44:28 -0700

    gitleaks3

--- .bin/install-gitleaks-linux-x64.sh
+++ .bin/install-gitleaks-linux-x64.sh
@@ -5,36 +5,40 @@ releases_api="https://api.github.com/repositories/119190187/releases/latest";
 releases_json="$(curl -s ${releases_api})";
 version=$(echo "${releases_json}" | jq -r ".name" | cut -c 2-); #Extracts the version from the json
 
-case "$OSTYPE" in
-  darwin*)  arch="darwin_amd64" ;; 
-  linux*)   arch="linux_amd64" ;;
-  solaris*) echo "Error: Solaris is not supported"; exit 1; ;;
-  bsd*)     echo "Error: BSD is not supported"; exit 1; ;;
-  msys*)    echo "Error: Windows is not supported"; exit 1; ;;
-  cygwin*)  echo "Error: Windows is still not supported"; exit 1; ;;
-  *)        echo "Error: unknown: $OSTYPE – definitely not supported"; exit 1; ;;
+# Detect OS architecture and set the arch variable
+case "$(uname -m)" in
+  'amd64')   arch="x64" ;;
+  'x86_64')  arch="x64" ;;
+  'i386')    arch="x32" ;;
+  'i686')    arch="x32" ;;
+  'armv6')   arch="armv6" ;;
+  'armv7')   arch="armv7" ;;
+  *)         echo "unsupported architecture"; exit 1 ;;
 esac
 
-# download the specified release
-download_url=$(echo "${releases_json}" | jq -r ".assets[] | select(.name | contains(\"${arch}\")) | .browser_download_url");
-wget "${download_url}";
+# Detect OS type and set the os variable
+case "$(uname -s)" in
+  Linux*)    os="linux" ;;
+  Darwin*)   os="darwin" ;;
+  CYGWIN*)   os="windows" ;;
+  MINGW*)    os="windows" ;;
+  MSYS*)     os="windows" ;;
+  *)         echo "unsupported OS"; exit 1 ;;
+esac
 
-# validate checksum
-download_url=$(echo "${releases_json}" | jq -r ".assets[] | select(.name | contains(\"checksums\")) | .browser_download_url");
-wget "${download_url}";
-sed -i.bak -n "/${arch}/p" gitleaks_${version}_checksums.txt
-shasum -a 256 "gitleaks_${version}_${arch}.tar.gz" --check;
+# Construct the filename to search for in the API response
+filename="gitleaks_${version}_${os}_${arch}.zip"
 
-# extract to current working directory
-tar -zxvf "gitleaks_${version}_${arch}.tar.gz";
+# Find the download URL for the desired asset
+download_url=$(echo "${releases_json}" | jq -r ".assets[] | select(.name==\"${filename}\") | .browser_download_url")
 
-# check version
-if [[ "$(./gitleaks --version)" != "v${version}" ]]; then
-  echo "Somehow we installed the wrong version...";
-  exit 1;
-fi
+# Download the asset
+wget "${download_url}"
 
-# cleanup
-rm gitleaks_*;
+# Extract the zip file
+unzip "gitleaks_${version}_${os}_${arch}.zip"
+
+# Cleanup
+rm gitleaks_${version}_${os}_${arch}.zip
 
 exit 0;
