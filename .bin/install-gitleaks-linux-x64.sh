e24158468165ceb60d0806d7e8e6a35ad6bd86e8
YaokunLin <yaokun1@ualberta.ca>
2023-07-07 11:37:47 -0700

    gitleaks latest version

--- .bin/install-gitleaks-linux-x64.sh
+++ .bin/install-gitleaks-linux-x64.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 set -xeuo pipefail;
 
-version="8.12.0";
-releases_api="https://api.github.com/repositories/119190187/releases/latest"; #Updated URL
+releases_api="https://api.github.com/repositories/119190187/releases/latest";
 releases_json="$(curl -s ${releases_api})";
+version=$(echo "${releases_json}" | jq -r ".name" | cut -c 2-); #Extracts the version from the json
 
 case "$OSTYPE" in
-  darwin*)  arch="darwin_x64" ;; 
-  linux*)   arch="linux_x64" ;;
+  darwin*)  arch="darwin_amd64" ;; 
+  linux*)   arch="linux_amd64" ;;
   solaris*) echo "Error: Solaris is not supported"; exit 1; ;;
   bsd*)     echo "Error: BSD is not supported"; exit 1; ;;
   msys*)    echo "Error: Windows is not supported"; exit 1; ;;
   cygwin*)  echo "Error: Windows is still not supported"; exit 1; ;;
   *)        echo "Error: unknown: $OSTYPE – definitely not supported"; exit 1; ;;
 esac
-
-# error if the specified release isn't latest
-if (echo "${releases_json}" | jq -e "(.name != \"v${version}\")"); then
-  echo "Error: the latest version of gitleaks is $(echo "${releases_json}" | jq "(.name)")";
-  exit 1;
-fi
 
 # download the specified release
 download_url=$(echo "${releases_json}" | jq -r ".assets[] | select(.name | contains(\"${arch}\")) | .browser_download_url");
@@ -29,13 +23,13 @@ wget "${download_url}";
 download_url=$(echo "${releases_json}" | jq -r ".assets[] | select(.name | contains(\"checksums\")) | .browser_download_url");
 wget "${download_url}";
 sed -i.bak -n "/${arch}/p" gitleaks_${version}_checksums.txt
-shasum -a 256 "gitleaks_${version}_checksums.txt" --check;
+shasum -a 256 "gitleaks_${version}_${arch}.tar.gz" --check;
 
 # extract to current working directory
-tar -zxvf "gitleaks_${version}_${arch}.tar.gz" gitleaks;
+tar -zxvf "gitleaks_${version}_${arch}.tar.gz";
 
 # check version
-if [[ "$(./gitleaks version)" != "${version}" ]]; then
+if [[ "$(./gitleaks --version)" != "v${version}" ]]; then
   echo "Somehow we installed the wrong version...";
   exit 1;
 fi
