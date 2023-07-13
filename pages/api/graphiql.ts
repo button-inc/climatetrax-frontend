7e5b42c56d669654a44af82e8f696040b7976848
YaokunLin <yaokun1@ualberta.ca>
2023-07-13 16:20:43 -0700

    test gh actions

--- pages/api/graphiql.ts
+++ pages/api/graphiql.ts
@@ -6,8 +6,8 @@ const databaseSchemaAdmin = process.env.DATABASE_SCHEMA_ADMIN || "";
 const databaseSchemaClean = process.env.DATABASE_SCHEMA_CLEAN || "";
 const databaseSchemaWorkspace = process.env.DATABASE_SCHEMA_WORKSPACE || "";
 const databasePW = "fake_pw_to_test_gitleak";
-const FAKE_AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE11";
-const FAKE_AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxSRfiCYEXAMPLEKEY11";
+const FAKE_AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE12";
+const FAKE_AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxSRfiCYEXAMPLEKEY12";
 
 
 const requestHandler = postgraphile(
